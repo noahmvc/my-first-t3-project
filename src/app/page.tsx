e651544dd474244aa49d333b9fdfcfb4862fd838
noahmvc <nas00001@mix.wvu.edu>
2024-08-09 13:51:41 -0400

    scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/891b9a08-f9f1-4b86-8eec-9cfd645c54d5-ibcluw.png",
+  "https://utfs.io/f/ddbfdf2c-b42b-4c37-ae12-c8e672907489-ibclvr.png",
+  "https://utfs.io/f/c24094c8-f975-4a86-8eac-1226cc2565de-nm33wo.png",
+  "https://utfs.io/f/c3358532-8871-488c-94b8-9e7d720853ae-ibcluw.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
